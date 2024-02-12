6645bfe2e3cd30caae04373be0a857eaffadc761
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2024-02-12 23:06:41 +0530

    about page structure

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -9,18 +9,27 @@ export const AboutPage = () => {
           <p className=" text-center text-2xl">My name is <strong>Bishal</strong> and I am a <strong>Full Stack Developer</strong> based in <strong>India</strong>.</p>
         </div>
 
-        <div className="flex justify-center">
-          <button>Resume</button>
-          <button>My Work</button>
+        <div className="flex justify-center gap-4">
+          <button className=" bg-[#3c4143] px-3 py-2 rounded-lg shadow-2xl shadow-gray-700" >Resume</button>
+          <button className="bg-[#3c4143] px-3 py-2 rounded-lg shadow-2xl shadow-gray-700">My Work</button>
         </div>
       </div>
-      
 
-      <div>
+
+      <div className="pt-8">
         <div>
-          <div className="border shadow-2xl p-5 rounded-lg bg-[#383d40]">
+          <div className="shadow-2xl p-5 rounded-lg bg-[#383d40]">
             <img className="rounded-lg shadow-2xl shadow-gray-700" src="https://portfolio-bishal-samanta.vercel.app/assets/propic.183ff822.jpeg" alt="" />
           </div>
+        </div>
+      </div>
+
+      <div className="pt-7">
+        <div className="pb-3">
+          <h3 className=" font-bold text-xl ml-1">About Me</h3>
+        </div>
+        <div className="rounded-lg shadow-2xl bg-[#3c4143] px-4 py-4 text-center">
+        I am a Passion-driven MERN developer with hands-on experience in developing and organizing projects, curious and detail-oriented with a keen interest to build interactive and user-friendly websites. Looking for an exciting opportunity to work as a full-stack developer that promotes long-term learning and growth alongside the organization.
         </div>
       </div>
         
