d7935494d9fb47ccd0ff9b117cf5e35311171b24
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2024-02-13 07:33:44 +0530

    added footer logo

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,16 +1,24 @@
-
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { IoLogoLinkedin } from "react-icons/io";
+import { FaGithub } from "react-icons/fa6";
 
 export const Footer = () => {
   return (
-    <div className="text-center">
-        <div className="flex flex-col gap-2">
-            <div className=" text-sm">Designed and Developed with ❤️ by Bishal Samanta</div>
-            <div className="text-[10px]">© 2023 - 2024 | Bishal Samanta | All rights reserved.</div>
+    <div className="text-center flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
+        <div className=" text-sm">
+          Designed and Developed with ❤️ by Bishal Samanta
         </div>
-        <div className="flex justify-center gap-4">
-            {/* <button className=" bg-[#3c4143] px-3 py-2 rounded-lg shadow-2xl shadow-black" >Resume</button>
-            <button className="bg-[#3c4143] px-3 py-2 rounded-lg shadow-2xl shadow-black">My Work</button> */}
+        <div className="text-[10px]">
+          © 2023 - 2024 | Bishal Samanta | All rights reserved.
         </div>
+      </div>
+      <div className="flex justify-center gap-2">
+     
+        <MdOutlineAlternateEmail className="text-xl text-[#d1cdc7]" />
+        <IoLogoLinkedin className="text-xl text-[#d1cdc7]" />
+        <FaGithub className="text-xl text-[#d1cdc7]" />
+      </div>
     </div>
-  )
-}
+  );
+};
