2122d50a5747df8a20ca55635a9ab52545c8de80
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2024-02-15 17:42:46 +0530

    make about page responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-
-
-import './App.css'
-import { Footer } from './components/common/Footer'
-import { Navbar } from './components/common/Navbar'
-import { AllRoutes } from './pages/AllRoutes'
-
+import "./App.css";
+import { Footer } from "./components/common/Footer";
+import { Navbar } from "./components/common/Navbar";
+import { AllRoutes } from "./pages/AllRoutes";
 
 function App() {
-  
-
   return (
     <>
       <div className="App bg-[#2e3234] text-[#d1cdc7] min-h-screen">
         <Navbar />
-        <div className='pt-4'>
+        <div className="pt-16">
           <AllRoutes />
         </div>
-        <div className='pt-10 pb-7'>
+        <div className="pt-10 pb-7 md:fixed md:w-full md:left-0 md:bottom-0 lg:static">
           <Footer />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
